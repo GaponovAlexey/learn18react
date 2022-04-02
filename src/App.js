8d7969862cdbe56f274c7d310e6d5095835df798
Kekelberi <alexei.gaponov@gmail.com>
2022-04-02 17:27:27 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 
 function App() {
+  const [state, setState] = useState(1)
+  const [value, setValue] = useState(1)
+
+  const update = () => {
+    setState((prev) => prev + 1)
+    setValue((prev) => prev + 1)
+  }
+  console.log('render')
+
   return (
-    <div >
-       start
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        {value} -{state}
+      </h1>
+      <button type='button' onClick={update}>
+        change
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
