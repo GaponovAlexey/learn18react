32ce2f1bea349f04f8c6bce3c2cc8cc5483d3ca8
Kekelberi <alexei.gaponov@gmail.com>
2022-04-02 18:34:57 +0300

    useTransition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import { useMemo, useState, } from 'react'
+import { useMemo, useState, useTransition } from 'react'
 import data from './exemple/data/phone.json'
 function App() {
   const [value, setvalue] = useState('')
+  const [FilterValue, setFilterValue] = useState('')
+
   const [items, setdata] = useState(data)
-  // const [] = useTransition()
+  const [isPending, startTransition] = useTransition()
 
   const filteredItems = useMemo(() => {
-    return items.filter((it) => it.phone.toLowerCase().includes(value))
-  }, [value])
+    return items.filter((it) => it.phone.toLowerCase().includes(FilterValue))
+  }, [FilterValue])
+
+  const onChangeValue = (e) => {
+    setvalue(e.target.value)
+    startTransition(() => {
+      setFilterValue(e.target.value)
+    })
+  }
 
   console.log('render')
   return (
     <div style={{ textAlign: 'center' }}>
+      {isPending && <h1>Loading...</h1>}
       <h1>
-        <input
-          type='text'
-          value={value}
-          onChange={(e) => setvalue(e.target.value)}
-        />
+        <input type='text' value={value} onChange={onChangeValue} />
       </h1>
       <div>
         {filteredItems.map((el, i) => (
